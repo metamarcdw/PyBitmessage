f2ef940908e80c5a3b4e007008824fb07e140b30
Marc Wood <metamarcdw@gmail.com>
2015-05-05 10:01:45 -0400

    Allow bootstrapping destinations from a JSON list hosted on an arbitrary eepsite

--- src/defaultKnownNodes.py
+++ src/defaultKnownNodes.py
@@ -7,11 +7,32 @@ import sys
 from time import strftime, localtime
 import shared
 
+def getBootstrapListFromEepsite():
+    print 'Getting some defaultKnownNodes from eepsite'
+    dest = 'CTJ6jOwdigYhfCi~jy3Zf5YCECdd4NiyZsgcQ1ncz~QCyDzy60mqpNFyaNFSYEsECeMzZXJy6BTFuYEOrhN3LLPbDt7cKCpB3p9OoB6QiQj7HuLBP8lOspTWagUbJk~IYo3D4Blp21cdp-~OguPaqKYyRDElBOFff91YcFF2SuGTEGl2bQu~v0WU2gpgIFACoC9bvv7b1FkdIizoB1A9NSv~VAjiC0~tNrNc3bOQJKhbEIkPP~E2IrmH78UdRrrYk0XKOELobcVkk6CNLqfZh7WZAjhVDMRBdBbwZwFs8D3s7r4nX8PX~1uD17w~a3nh8XlvKETUjmRl~ZlxeDUTdkESuNI40r6a6JXMfTeMFKDT2Cr9xzDMqRSUAU56MMpX5he5o82Clv8XXiB64Tg2QVGToEKzD-UWSLaYTg0qac~lGPGizYEzEJfHyZS1trBsnd6lznF0Exn8MSthRVawT6E3rrGhU6E1D-Ky1SyqRkiU2kHXx81gDHmG-t99BFpuBQAEAAEAAA=='
+    S = socket.socket('', socket.SOCK_STREAM)  
+    S.connect(dest)
+    S.send('GET /bootstrap.json HTTP/1.0\r\n\r\n')          # Send request
+    f = S.makefile()                          # File object
+    S.close()
+    
+    while True:                               # Read header
+        line = f.readline().strip()           # Read a line
+        if line == '': break                  # Content begins
+    
+    str = f.read()
+    import json
+    return json.loads(str)                    # Read file object
+
 def createDefaultKnownNodes(appdata):
     ############## Stream 1 ################
     stream1 = {}
     #stream1[shared.Peer('VXVZRtml-XDgkwFcehckXBQ1qOx8whwjYlPZnKyIp3L5OFhwF6moUjkAoN~4J5TmdBLP5jxoOEwe5pC6TcgkKAvEXLqGvb607LPr9XhhWdgfHFyfcEG1zGhMziisOSHwmnUAjlvd5FT9H7ouv2on5JvLAHRiqe-vO0Ifz~dnkQyhd-IouWArdTlXQqhm7ArMS1-vHQKaslktY9BrFS8ZxKojbAMxcrBrt-9IND1f9-KpRBwtKp0Hup6jzIk3cNGbP4eadZ3F-Zic6oy-ktsH0iz5FBKmpMdc36SQDG8rReMjngKZntl4OhxjAZ7eYLllA6T3X5wdICkoqNJEobByGx9TEYXq6bVlyp7aoxGuB8~piqJWoCqbgfcIDUznP050YoCKp3Uk6u9DmROP4pckzg910FdKSF3TRlebKRRzB7KHWXV~CY3xZEp8CKblBljJEw3FNv0IZ5Guq0tNi9bjs6uXtY1IPviEN9cVfmT3EZ5WK8b~3JdvZrDGKoWAJkRAAAAA')] = int(time.time())
-    
+
+    eepsite_list = getBootstrapListFromEepsite()
+    for dest in eepsite_list:
+        stream1[shared.Peer(dest)] = int(time.time())
+        
     ############# Stream 2 #################
     stream2 = {}
     # None yet
